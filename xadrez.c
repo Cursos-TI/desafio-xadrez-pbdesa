41739b1007587cb20d1a8d0385a91971b0190137
pbdesa <57603352+pbdesa@users.noreply.github.com>
2025-10-25 19:52:39 +0000

    finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
+// Desafio de Xadrez - MateCheck - Pablo Francisco de Sa
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
